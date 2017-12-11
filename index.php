c5e9b1751ad2f3a0e608898c88bd8a08749f05ce
jerzabongco <34363144+jerzabongco@users.noreply.github.com>
2017-12-11 00:07:16 -0800

    Update index.php

--- index.php
+++ index.php
@@ -9,11 +9,11 @@
 				<label class = "text" style = "position: absolute;top: 20px; left:550px;text-align: center; font-size: 20px;">USE CASE DIAGRAM</label>
 				
 			</div>
-			<div class = "imageBase" style="position:absolute; top: 90px; left: 150px; height:550px; width: 1000px;">
+			<div class = "imageBase" style="position:absolute; top: 90px; left: 150px; height:550px; width: 1030px;">
 				<img src="usecase.png" width="100%" height="100%">
 			</div>
-		<a class = "prev" href="" style="position:absolute; height:50px; left:30px; width: 100px; bottom: 20px; background-color: white; text-decoration:none; font-size:25px;color: black; text-indent:5%; ">Previous Photo</a>
-		<a class = "next" href="" style="position:absolute; height:50px; right:30px; width: 100px; bottom: 20px; background-color: white; text-decoration:none; font-size:15px;color: black; text-indent:5%; ">Next Photo</a>
+		<a class = "prev" href="" style="position:absolute; height:50px; left:30px; width: 100px; bottom: 20px; background-color: white; text-decoration:none; font-size:10px;color: black; text-indent:5%; ">Previous Photo</a>
+		<a class = "next" href="" style="position:absolute; height:50px; right:30px; width: 100px; bottom: 20px; background-color: white; text-decoration:none; font-size:10px;color: black; text-indent:5%; ">Next Photo</a>
 		</div>
 	</body>
 </html>
