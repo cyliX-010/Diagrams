0869dbc5c7d581c6fac06fe705b978e8cef26bef
jerzabongco <34363144+jerzabongco@users.noreply.github.com>
2017-12-11 00:12:38 -0800

    Update index.php

--- index.php
+++ index.php
@@ -6,14 +6,22 @@
 	<body>
 		<div class = 'base' style = 'position:absolute; top:0px; left: 0px; height:100%; width:100%; background-color: gray;'>
 			<div class = "header" style = 'position:absolute;top:0px; left:0px; height:70px; width: 100%; background-color: red;'>
-				<label class = "text" style = "position: absolute;top: 20px; left:550px;text-align: center; font-size: 20px;">USE CASE DIAGRAM</label>
-				
+				<label id = "text" style = "position: absolute;top: 20px; left:550px;text-align: center; font-size: 20px;">USE CASE DIAGRAM</label>
+				<label id = "text1" style = "position: absolute;top: 20px; left:550px;text-align: center; font-size: 20px;">CLASS DIAGRAM</label>
 			</div>
 			<div class = "imageBase" style="position:absolute; top: 90px; left: 150px; height:550px; width: 1030px;">
 				<img src="usecase.png" width="100%" height="100%">
 			</div>
-		<a class = "prev" href="" style="position:absolute; height:50px; left:30px; width: 100px; bottom: 20px; background-color: white; text-decoration:none; font-size:10px;color: black; text-indent:5%; ">Previous Photo</a>
-		<a class = "next" href="" style="position:absolute; height:50px; right:30px; width: 100px; bottom: 20px; background-color: white; text-decoration:none; font-size:10px;color: black; text-indent:5%; ">Next Photo</a>
+		<a id = "prev" href="" style="position:absolute; height:50px; left:30px; width: 100px; bottom: 20px; background-color: white; text-decoration:none; font-size:10px;color: black; text-indent:5%; ">Previous Photo</a>
+		<a id = "next" href="" style="position:absolute; height:50px; right:30px; width: 100px; bottom: 20px; background-color: white; text-decoration:none; font-size:10px;color: black; text-indent:5%; ">Next Photo</a>
 		</div>
 	</body>
 </html>
+<script>
+	$("#next").click(function(){
+		document.getElementById(text).style.display="none";
+		document.getElementById(text1).style.display="display";
+		
+	});
+	
+</script>
