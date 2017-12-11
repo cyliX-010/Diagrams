8b8cff37baa3db5b0dc0e4959a272930afb0efc2
jerzabongco <34363144+jerzabongco@users.noreply.github.com>
2017-12-11 00:16:05 -0800

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 				<label id = "text" style = "position: absolute;top: 20px; left:550px;text-align: center; font-size: 20px;">USE CASE DIAGRAM</label>
 				<label id = "text1" style = "position: absolute;top: 20px; left:550px;text-align: center; font-size: 20px; display: none;">CLASS DIAGRAM</label>
 			</div>
-			<div class = "imageBase" style="position:absolute; top: 90px; left: 170px; height:550px; width: 1000px;">
+			<div class = "imageBase" style="position:absolute; top: 90px; left: 190px; height:550px; width: 980px;">
 				<img src="usecase.png" width="100%" height="100%">
 			</div>
 		<a id = "prev" href="" style="position:absolute; height:50px; left:30px; width: 150px; bottom: 20px; background-color: white; text-decoration:none; font-size:10px;color: black; text-indent:5%; ">Previous Photo</a>
