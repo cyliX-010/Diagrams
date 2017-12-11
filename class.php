1a20660cd507306889b26e24ec89ba603b405ef9
jerzabongco <34363144+jerzabongco@users.noreply.github.com>
2017-12-11 02:59:43 -0800

    Update class.php

--- class.php
+++ class.php
@@ -3,9 +3,9 @@
 	<head></head>
 	<body>
 		<label id="use" style="position:absolute; left:400px; font-size:20px; ">CLASS DIAGRAM</label>
-		<img class = "usecase" src="class1.jpg" width = "99%" height = "100%" style = "position:relative; top: 40px;">
+		<img class = "usecase" src="class1.png" width = "99%" height = "100%" style = "position:relative; top: 40px;">
 		<img class = "usecase" src="class2.png" width = "99%" height = "100%" style = "position:relative; top: 40px; ">
-		<img class = "usecase" src="class3.jpeg" width = "99%" height = "100%" style = "position:relative; top: 40px; ">
+		<img class = "usecase" src="class3.png" width = "99%" height = "100%" style = "position:relative; top: 40px; ">
 	</body>
 </html>
 
