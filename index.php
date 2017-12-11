08ff5aee97c671f4dfb6b31fc15fe713257d363c
jerzabongco <34363144+jerzabongco@users.noreply.github.com>
2017-12-10 23:40:18 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+</head>
+	<body>
+		<div class = 'base' style = 'position:absolute; top:0px; left: 0px; height:100%; width:100%; background-color: gray;'>
+			<div class = "header" style = 'position:absolute;top:0px; left:0px; height:70px; width: 100%; background-color: red;'>
+				<label>USE CASE DIAGRAM</label>
+				
+			</div>
+			<div class = "imageBase" style="position:absolute; top: 200px; left: 200px; height:300px; width: 400px;">
+				<img src="usecase.png" width="100%" height="100%">
+			</div>
+		<a class = "next" href="" style="position:absolute; height:70px; right:100px; width: 150px; bottom: 100px; background-color: white;">Next Photo</a>
+		</div>
+	</body>
+</html>
